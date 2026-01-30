548c2e97a53740ef23acd2f877f6b4f041baaa38
Eugene-TechnoNinja <technoninja@ukr.net>
2026-01-30 22:15:19 +0200

    branch marge websocket

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <raylib.h>
 #include <string>
-#include "../external/httplib/httplib.h"
 #include "../external/inih/INIReader.h"
+#include <ixwebsocket/IXWebSocket.h>
+#include <mutex>
 
 int main() {
+  std::mutex mut;
   INIReader reader("settings.ini");
 
   if (reader.ParseError() < 0) {
@@ -12,31 +14,37 @@ int main() {
     return 1;
   }
 
-  std::string host = reader.Get("server", "host", "localhost");
+  std::string host = reader.Get("server", "host", "0.0.0.0");
   unsigned port = reader.GetInteger("server", "port", 5000);
   std::string path = reader.Get("server", "path", "/data");
 
+  ix::WebSocket webSocket;
+  std::string url("ws://" + host + ":" + std::to_string(port));
+  webSocket.setUrl(url);
+  webSocket.setPingInterval(45);
+
   std::string response;
-  std::cout << "Hello! Client!" << '\n';
+  webSocket.setOnMessageCallback([&](const ix::WebSocketMessagePtr &msg) {
+    if (msg->type == ix::WebSocketMessageType::Message) {
+      mut.lock();
+      response = msg->str;
+      mut.unlock();
+    }
+  });
 
-  std::string url = "http://" + host + ":" + std::to_string(port);
-  httplib::Client cli(url);
-  if (auto res = cli.Get(path)) {
-    std::cout << "[*] Status: " << res->status << "[*] Body: " << res->body
-              << '\n';
-    response = res->body;
-  }
+  webSocket.start();
 
-  InitWindow(1024, 768, "Client DT");
+  InitWindow(1024, 768, "Client Digital House");
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
-    DrawText(response.c_str(), 20, 100, 27, GREEN);
+    DrawText(response.c_str(), 20, 100, 27, YELLOW);
     EndDrawing();
   }
 
   CloseWindow();
+  webSocket.stop();
   return 0;
 }
